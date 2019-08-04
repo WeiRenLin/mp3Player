448a7514eca125dddc37c9d706eea570813062f1
s3211t <s3211t@gmail.com>
2019-08-04 23:08:14 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "/mp3Player" : "/"
+    publicPath: process.env.NODE_ENV === 'production'
+    ? '/mp3Player/public/index.html'
+    : '/',
    };
